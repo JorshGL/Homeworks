33fc9dbcc22feca21dfca742197bd601bc779144
JorshGL <jorge.gl0905@gmail.com>
2023-02-25 17:40:27 -0500

    a

--- project/src/components/FirstApp.jsx
+++ project/src/components/FirstApp.jsx
@@ -1,33 +1,13 @@
 import React from "react";
 import { useState } from "react";
 
-const FirstApp = ({ value }) => {
-  const [counter, setCounter] = useState(value);
-
-  const handleAdd = () => {
-    setCounter(counter + 1);
-  };
-
-  const handleSubstract = () => {
-    setCounter(counter - 1);
-  };
-
-  const handleReset = () => {
-    setCounter(value);
-  };
+const FirstApp = () => {
 
   return (
-    <div className="flex flex-col space-y-4">
-      <div className="text-5xl font-bold">Counter</div>
-      <div className="flex flex-col space-y-4">
-        <span className="text-xl font-ligth">{counter}</span>
-        <div className="flex space-x-6">
-          <button onClick={() => handleAdd()}> + 1 </button>
-          <button onClick={() => handleSubstract()}> - 1 </button>
-          <button onClick={() => handleReset()}> Reset </button>
-        </div>
-      </div>
-    </div>
+    <>
+      <h1>A new title</h1>
+      <span>10</span>
+    </>
   );
 };
 
