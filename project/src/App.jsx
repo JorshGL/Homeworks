ee8a8e23a863521f6803c8dea5b7125c12f9004c
JorshGL <jorge.gl0905@gmail.com>
2023-04-19 00:24:49 -0500

    challenge 13

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,7 +1,52 @@
-import './App.css'
+import { useDispatch, useSelector } from "react-redux";
 
-function App() {
-  return <h1>Hola Mundo</h1>
-}
+import React, { useState } from "react";
+import {
+  decrement,
+  increment,
+  incrementBy,
+} from "./store/slices/counter/counterSlice";
 
-export default App
+const App = () => {
+  const { counter } = useSelector((state) => state.counter);
+  const [value, setValue] = useState(0);
+
+  const dispatch = useDispatch();
+
+  return (
+    <div className="flex flex-col items-center justify-center bg-black h-screen w-screen text-white mx-0 my-0">
+      <span className="font-bold font-mono text-2xl">
+        Counter is: {counter}
+      </span>
+      <div className="flex flex-col w-1/3 gap-2 my-6">
+        <button
+          className="bg-neutral-900 py-2 rounded-md"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <button
+          className="bg-neutral-900 py-2 rounded-md"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+        <div className="flex gap-2">
+          <button
+            className="bg-neutral-900 w-1/2 py-2 rounded-md"
+            onClick={() => dispatch(incrementBy(value))}
+          >
+            Increment by
+          </button>
+          <input
+            className="bg-neutral-600 rounded-md w-1/2 p-2 focus:outline-none"
+            type="number"
+            onChange={(e) => setValue(Number(e.target.value))}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
--- project/src/main.jsx
+++ project/src/main.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
+import { Provider } from "react-redux";
 import "./index.css";
-import MainApp from "./components/MainApp";
-import { BrowserRouter } from "react-router-dom";
+import { store } from "./store/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <MainApp />
-    </BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
